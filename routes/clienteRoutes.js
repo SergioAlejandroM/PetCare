0cb8b84de18815ec4683f6719f949ef32c807e39
Julian Acuña <142070366+JulianAcun@users.noreply.github.com>
2026-09-24 18:34:34 -0500

    Delete routes directory

--- routes/clienteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { crearCliente } = require('../controllers/clienteController');
-
-router.post('/', crearCliente);
-
-module.exports = router;
