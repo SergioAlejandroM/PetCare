22378eadeb8438ee8ebddf64544017fb6fd9d561
Sergio Manrique <alejocoder03@gmail.com>
2026-09-24 10:30:50 -0500

    feat: clienteController y clienteRoutes completado junto con pruebas en postman

--- backend/controllers/clienteController.js
+++ backend/controllers/clienteController.js
@@ -3,9 +3,38 @@ const {asyncHandler, ApiError} = require('../middleware/errorHandler');
 
 // POST
 
-const crearCliente = asyncHandler( async (req, res) => {
+const crearCliente = asyncHandler(async (req, res) => {
     const cliente = await Cliente.create(req.body);
     req.status(201).json({ ok: true, data: cliente});
 });
 
-module.exports = { crearCliente };
+const listarClientes = asyncHandler(async(req, res) =>{
+    const clientes = await Cliente.find({activo: true});
+    res.json({ok: true, data: clientes})
+});
+
+const obtenerCliente = asyncHandler(async(req, res) => {
+    const cliente = await Cliente.findById(req.params.id);
+    if(!cliente) throw new ApiError(404,'Cliente no encontrado');
+    res.json({ok: true, data: cliente});
+});
+
+const actualizarCliente = asyncHandler(async(req, res) => {
+    const cliente = await Cliente.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+    if(!cliente) throw new ApiError(404,'Cliente no encontrado');
+    res.json({ok: true, data: cliente});
+});
+
+const desactivarCliente = asyncHandler(async(req, res) => {
+    const cliente = await Cliente.findByIdAndUpdate(req.params.id, 
+        { activo : false },
+        { new: true}
+    );
+    if(!cliente) throw new ApiError(404,'Cliente no encontrado');
+    res.json({ok: true, data: cliente});
+});
+
+module.exports = { crearCliente, listarClientes, obtenerCliente, actualizarCliente, desactivarCliente};
--- backend/routes/clienteRoutes.js
+++ backend/routes/clienteRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { crearCliente } = require('../controllers/clienteController');
+const { crearCliente, listarClientes, obtenerCliente, actualizarCliente, desactivarCliente} = require('../controllers/clienteController');
 
 router.post('/', crearCliente);
+router.get('/', listarClientes);
+router.get('/:id', obtenerCliente);
+router.put('/:id', actualizarCliente);
+router.patch('/:id/desactivar', desactivarCliente);
 
 module.exports = router;
